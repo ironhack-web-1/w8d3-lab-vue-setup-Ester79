bc6e17ef83d2f3732a7ae4e895ac1227a7ba48f1
Ester79 <esterramoscuervo@hotmail.com>
2022-08-27 16:21:32 +0200

    setup

---
+++ proyecto/src/components/AboutUs.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="styleAboutMe">
+        <h1>{{aboutMe}}</h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                aboutMe: "Hi I'm Ester!"
+            };
+        },        
+    }
+</script>
+
+<style scoped>
+.styleAboutMe {
+    color: blue;
+    font-family:Arial, Helvetica, sans-serif;
+    border: 3px solid darkblue;
+    background-color: greenyellow;
+    width: fit-content;
+    padding: 0px 25px 0px 25px;   
+}
+.styleAboutMe:hover{
+    background-color: aqua;
+    color: white;
+    width:50%;
+    padding-left: 40%;
+    margin-left: auto;
+    margin-right: auto;   
+}
+
+</style>
